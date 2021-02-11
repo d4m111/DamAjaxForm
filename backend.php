e074e8a3d1718702682118986f92c077f62e6097
damian <dcurcio@telecentro.net.ar>
2021-02-11 19:02:57 -0300

    agrego ejemplo de ajaxCall

---
+++ backend.php
@@ -0,0 +1,9 @@
+<?php
+
+// header('HTTP/1.1 500 custom error message');
+
+if($_SERVER['PHP_AUTH_USER'] != 'username' || $_SERVER['PHP_AUTH_PW'] != 'password') header('HTTP/1.1 401');
+
+// var_export($_REQUEST);
+
+echo json_encode($_REQUEST);
